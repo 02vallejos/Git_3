c8c5e00739cab051847d208d622c9d0ee5b4fcef
cvallejos <cdvallejos02@gmail.com>
2023-04-10 21:15:12 -0300

    Ver 1.1

---
+++ git_3/src/git_3/entidades/Animales.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_3.entidades;
+
+/**
+ *
+ * @author Cesar Vallejos
+ */
+public class Animales {
+    private String nombre;
+    private int edad;
+
+    public Animales(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Animales() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
